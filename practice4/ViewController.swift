0c196d2c91f3b27a9f837bacb825d9022d483c39
Atsushi.Taketsuji <at.0130.145@gmail.com>
2021-04-25 13:33:37 +0900

    Initial Commit

---
+++ practice4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice4
+//
+//  Created by 竹辻篤志 on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
