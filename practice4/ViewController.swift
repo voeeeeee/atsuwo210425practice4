a7607f3d4313a450e8c3f44d5c7a20fdb18d605b
Atsushi.Taketsuji <at.0130.145@gmail.com>
2021-04-25 13:48:12 +0900

    complete practice4

--- practice4/ViewController.swift
+++ practice4/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countNumberLabel: UILabel!
+    var number:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countNumberLabel.text = "0"
     }
 
-
+    @IBAction func countUpButton(_ sender: UIButton) {
+        number += 1
+        countNumberLabel.text = String(Int(number))
+        
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        number = 0
+        countNumberLabel.text = String(Int(number))
+        
+    }
+    
 }
 
